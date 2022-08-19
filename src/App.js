1d0317723e4dc3d3ba200b0edfe1c852db381ac5
michelle okolie <michelleokolie11@gmail.com>
2022-08-19 01:20:52 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Lobby from "./components/Lobby";
+import Uniswap from "./components/Uniswap";
+import Docs from "./components/Docs";
+import Treasury from "./components/Treasury";
+
 function App() {
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar />
+      {/* <Uniswap />
+      <Lobby /> */}
+
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/uniswap" element={<Uniswap />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/treasury" element={<Treasury />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Uniswap.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Uniswap = () => {
+  return <div>Uniswap</div>;
+};
+
+export default Uniswap;
